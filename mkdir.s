043a569bf1349092a021fb3857917bf2336b4600
itagagaki <itagagaki@gmail.com>
2023-09-21 20:20:31 +0900

    1.1

--- mkdir.s
+++ mkdir.s
@@ -1,20 +1,30 @@
 * mkdir - make directory
 *
 * Itagaki Fumihiko  9-Jul-91  Create.
+* 1.0
+* Itagaki Fumihiko 22-Aug-92  strip_excessive_slashes
+* Itagaki Fumihiko 17-Sep-92  非再帰化、mkdir -p foo/../bar/baz が可能。
+* Itagaki Fumihiko 24-Sep-92  ドライブを予め検査するのはやめた。
+*                             どうしたって完全にはチェックできないので。
+* 1.1
 *
 * Usage: mkdir [ -p ] <パス名> ...
 
 .include doscall.h
 .include error.h
+.include limits.h
+.include stat.h
 .include chrcode.h
 
 .xref DecodeHUPAIR
+.xref issjis
 .xref strlen
 .xref strfor1
 .xref headtail
-.xref drvchkp
+.xref cat_pathname
+.xref strip_excessive_slashes
 
-STACKSIZE	equ	2048
+STACKSIZE	equ	256
 
 .text
 start:
@@ -73,64 +83,41 @@ decode_opt_done:
 
 		moveq	#0,d6				*  D6.W : エラー・コード
 loop:
-		movea.l	a0,a2
+		movea.l	a0,a1
 		bsr	strfor1
-		exg	a0,a2				*  A2 : 次の引数
-
-		bsr	drvchkp
-		bmi	fail
-
+		exg	a0,a1				*  A1 : 次の引数
+		move.l	a1,-(a7)
+		bsr	strip_excessive_slashes
 		bsr	mkdir
-		bpl	next
-fail:
-		move.l	a0,-(a7)
-		bsr	werror_myname
-		cmp.l	#ENODIR,d0
-		beq	fail_nodir
-
-		lea	msg_directory(pc),a0
-		bsr	werror
-		movea.l	(a7),a0
-		bsr	werror
-		lea	msg_failed(pc),a0
-		bsr	werror
-		bra	fail_perror
-
-fail_nodir:
-		movea.l	(a7),a0
-		move.l	d0,-(a7)
-		bsr	headtail
-		move.l	(a7)+,d0
-		bsr	werror_2
-fail_perror:
-		bsr	perror
 		movea.l	(a7)+,a0
-		moveq	#2,d6
-next:
-		movea.l	a2,a0
 		subq.l	#1,d7
 		bne	loop
 exit_program:
 		move.w	d6,-(a7)
 		DOS	_EXIT2
 
-
 bad_option:
-		bsr	werror_myname
-		lea	msg_illegal_option(pc),a0
-		bsr	werror
-		move.w	d0,-(a7)
-		move.l	#1,-(a7)
-		pea	5(a7)
+		moveq	#1,d1
+		tst.b	(a0)
+		beq	bad_option_1
+
+		bsr	issjis
+		bne	bad_option_1
+
+		moveq	#2,d1
+bad_option_1:
+		move.l	d1,-(a7)
+		pea	-1(a0)
 		move.w	#2,-(a7)
+		lea	msg_illegal_option(pc),a0
+		bsr	werror_myname_and_msg
 		DOS	_WRITE
-		lea	12(a7),a7
+		lea	10(a7),a7
 		bra	usage
 
 too_few_args:
-		bsr	werror_myname
 		lea	msg_too_few_args(pc),a0
-		bsr	werror
+		bsr	werror_myname_and_msg
 usage:
 		lea	msg_usage(pc),a0
 		bsr	werror
@@ -138,32 +125,135 @@ usage:
 		bra	exit_program
 
 insufficient_memory:
-		bsr	werror_myname
 		lea	msg_no_memory(pc),a0
-		bsr	werror
+		bsr	werror_myname_and_msg
 		moveq	#3,d6
 		bra	exit_program
 *****************************************************************
+mkdir:
+		tst.b	d5
+		beq	mkdir_one
+****************
+		movea.l	a0,a1
+make_path_loop:
+		move.b	(a1)+,d0
+		beq	make_path_find_slash_done
+
+		cmp.b	#'/',d0
+		beq	make_path_find_slash_done
+
+		cmp.b	#'\',d0
+		beq	make_path_find_slash_done
+
+		bsr	issjis
+		bne	make_path_loop
+
+		move.b	(a1)+,d0
+		bne	make_path_loop
+make_path_find_slash_done:
+		move.b	d0,d1
+		clr.b	-(a1)
+		bsr	is_directory
+		bmi	make_path_too_long_path
+		bne	make_path_next
+
+		bsr	mkdir_one
+		bmi	mkdir_return
+make_path_next:
+		move.b	d1,(a1)+
+		bne	make_path_loop
+
+		rts
+
+make_path_too_long_path:
+		moveq	#2,d6
+		bsr	werror_myname_and_msg
+		lea	msg_too_long_pathname(pc),a0
+		bra	werror
+****************
+mkdir_one:
+		move.l	a0,-(a7)
+		DOS	_MKDIR
+		addq.l	#4,a7
+		tst.l	d0
+		bpl	mkdir_return
+mkdir_error:
+		bsr	werror_myname
+		cmp.l	#ENODIR,d0
+		beq	mkdir_fail_nodir
+
+		move.l	a0,-(a7)
+		lea	msg_directory(pc),a0
+		bsr	werror
+		movea.l	(a7),a0
+		bsr	werror
+		lea	msg_failed(pc),a0
+		bsr	werror
+		movea.l	(a7)+,a0
+		bra	mkdir_fail_perror
+
+mkdir_fail_nodir:
+		move.l	d0,-(a7)
+		bsr	headtail
+		move.l	(a7)+,d0
+		bsr	werror_2
+mkdir_fail_perror:
+		bsr	perror				*  ここで D6.L に 2 がセットされる
+		tst.l	d0
+mkdir_return:
+		rts
+*****************************************************************
+* is_directory - 名前がディレクトリであるかどうかを調べる
+*
+* CALL
+*      A0     名前
+*
+* RETURN
+*      D0.L   名前/*.* が長すぎるならば -1．
+*             そうでなければ，名前がディレクトリならば 1，さもなくば 0
+*
+*      CCR    TST.L D0
+*****************************************************************
+is_directory:
+		movem.l	a0-a3,-(a7)
+		tst.b	(a0)
+		beq	is_directory_false
+
+		movea.l	a0,a1
+		lea	pathname_buf(pc),a0
+		lea	dos_wildcard_all(pc),a2
+		bsr	cat_pathname
+		bmi	is_directory_return
+
+		move.w	#MODEVAL_ALL,-(a7)		*  すべてのエントリを検索する
+		move.l	a0,-(a7)
+		pea	filesbuf(pc)
+		DOS	_FILES
+		lea	10(a7),a7
+		tst.l	d0
+		bpl	is_directory_true
+
+		cmp.l	#ENOFILE,d0
+		beq	is_directory_true
+is_directory_false:
+		moveq	#0,d0
+		bra	is_directory_return
+
+is_directory_true:
+		moveq	#1,d0
+is_directory_return:
+		movem.l	(a7)+,a0-a3
+		rts
+*****************************************************************
 perror:
+		movem.l	d0/a0,-(a7)
 		not.l	d0		* -1 -> 0, -2 -> 1, ...
 		cmp.l	#25,d0
 		bls	perror_2
 
-		cmp.l	#256,d0
-		blo	perror_1
-
-		sub.l	#256,d0
-		cmp.l	#4,d0
-		bhi	perror_1
-
-		lea	perror_table_2(pc),a0
-		bra	perror_3
-
-perror_1:
-		moveq	#25,d0
+		moveq	#0,d0
 perror_2:
 		lea	perror_table(pc),a0
-perror_3:
 		lsl.l	#1,d0
 		move.w	(a0,d0.l),d0
 		bmi	perror_4
@@ -173,10 +263,13 @@ perror_3:
 		bsr	werror
 perror_4:
 		lea	msg_newline(pc),a0
-		bra	werror
+		bsr	werror
+		movem.l	(a7)+,d0/a0
+		moveq	#2,d6
+		rts
 *****************************************************************
-werror_myname:
-		lea	msg_myname(pc),a0
+werror_myname_and_msg:
+		bsr	werror_myname
 werror:
 		movea.l	a0,a1
 werror_1:
@@ -194,59 +287,17 @@ werror_2:
 		move.l	(a7)+,d0
 		rts
 *****************************************************************
-mkdir:
-		movem.l	d1/a1,-(a7)
-
-	*  目的のディレクトリを作ってみる
-
-		bsr	do_mkdir
-		bpl	mkdir_return			*  成功
-
-		tst.b	d5				*  -p が指定されて
-		beq	mkdir_return			*  いないならば失敗とする
-
-		cmp.l	#ENODIR,d0			*  「パス名の途中のディレクトリが無い」
-		bne	mkdir_return			*  以外ならば失敗
-
-	*  パス名の途中のディレクトリが無い ... まず親ディレクトリを作る
-
-		move.l	d0,-(a7)
-		bsr	headtail
-		move.l	(a7)+,d0
-		cmpa.l	a0,a1
-		beq	mkdir_return			*  目的のディレクトリに親は無い
-
-		move.b	-(a1),d1
-		cmp.b	#'/',d1
-		beq	mkdir_recurse
-
-		cmp.b	#'\',d1
-		bne	mkdir_return			*  目的のディレクトリに親は無い
-mkdir_recurse:
-		clr.b	(a1)
-		bsr	mkdir		**  再帰  **
-		bmi	mkdir_return			*  親の作成に失敗
-
-	*  親ディレクトリの作成は成功した ... いよいよ目的のディレクトリを作る
-
-		move.b	d1,(a1)
-		bsr	do_mkdir
-mkdir_return:
-		movem.l	(a7)+,d1/a1
-		tst.l	d0
-		rts
-*****************************************************************
-do_mkdir:
+werror_myname:
 		move.l	a0,-(a7)
-		DOS	_MKDIR
-		addq.l	#4,a7
-		move.l	d0,d3
+		lea	msg_myname(pc),a0
+		bsr	werror
+		movea.l	(a7)+,a0
 		rts
 *****************************************************************
 .data
 
 	dc.b	0
-	dc.b	'## mkdir 1.0 ##  Copyright(C)1992 by Itagaki Fumihiko',0
+	dc.b	'## mkdir 1.1 ##  Copyright(C)1992 by Itagaki Fumihiko',0
 
 .even
 perror_table:
@@ -277,14 +328,6 @@ perror_table:
 	dc.w	-1					*  24 (-25)
 	dc.w	-1					*  25 (-26)
 
-.even
-perror_table_2:
-	dc.w	msg_bad_drivename-sys_errmsgs		* 256 (-257)
-	dc.w	msg_no_drive-sys_errmsgs		* 257 (-258)
-	dc.w	msg_no_media_in_drive-sys_errmsgs	* 258 (-259)
-	dc.w	msg_media_set_miss-sys_errmsgs		* 259 (-260)
-	dc.w	msg_drive_not_ready-sys_errmsgs		* 260 (-261)
-
 sys_errmsgs:
 msg_nodir:		dc.b	': このようなディレクトリはありません',0
 msg_bad_name:		dc.b	'; 名前が無効です',0
@@ -293,22 +336,22 @@ msg_write_disabled:	dc.b	'; 書き込みが許可されていません',0
 msg_directory_exists:	dc.b	'; すでに存在しています',0
 msg_directory_full:	dc.b	'; ディレクトリが満杯です',0
 msg_disk_full:		dc.b	'; ディスクが満杯です',0
-msg_bad_drivename:	dc.b	'; ドライブ名が無効です',0
-msg_no_drive:		dc.b	'; ドライブがありません',0
-msg_no_media_in_drive:	dc.b	'; ドライブにメディアがセットされていません',0
-msg_media_set_miss:	dc.b	'; ドライブにメディアが正しくセットされていません',0
-msg_drive_not_ready:	dc.b	'; ドライブの準備ができていません',0
 
 msg_myname:		dc.b	'mkdir: ',0
 msg_no_memory:		dc.b	'メモリが足りません',CR,LF,0
-msg_directory:		dc.b	' ディレクトリ "',0
-msg_failed:		dc.b	'" の作成に失敗しました',0
+msg_directory:		dc.b	'ディレクトリ“',0
+msg_failed:		dc.b	'”の作成に失敗しました',0
+msg_too_long_pathname:	dc.b	': パス名が長過ぎます',CR,LF,0
 msg_illegal_option:	dc.b	'不正なオプション -- ',0
 msg_too_few_args:	dc.b	'引数が足りません',0
 msg_usage:		dc.b	CR,LF,'使用法:  mkdir [-p] [-] <パス名> ...'
 msg_newline:		dc.b	CR,LF,0
+dos_wildcard_all:	dc.b	'*.*',0
 *****************************************************************
 .bss
+.even
+filesbuf:	ds.b	STATBUFSIZE
+pathname_buf:	ds.b	MAXPATH+1
 .even
 		ds.b	STACKSIZE
 .even
